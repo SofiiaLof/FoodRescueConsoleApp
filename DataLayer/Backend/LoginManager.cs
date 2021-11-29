9510a3fbc9de6e9fe112dfee45747d29d8f389fd
hargaaya <hargaaya10@gmail.com>
2021-11-29 10:47:43 +0100

    Created Class: LoginManager, Created Method: Login, Created Method: ChangePassword.

---
+++ DataLayer/Backend/LoginManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataLayer.Model;
+using DataLayer.Data;
+
+namespace DataLayer.Backend
+{
+    internal class LoginManager
+    {
+        public User? Login(string Username, string Password)
+        {
+
+            using var ctx = new FoodRescDbContext();
+
+            var query = ctx.Users
+                .Where(c => c.UserPrivateInfo.Username == Username && c.UserPrivateInfo.Password == Password)
+                .Include(x => x.UserPrivateInfo)
+                .FirstOrDefault();
+
+            if (query != null) return query;
+
+            return null;
+        }
+
+        public bool ChangePassword(string Username, string Password, string NewPassword)
+        {
+            using var ctx = new FoodRescDbContext();
+
+            var query = ctx.Users
+                .Where(c => c.UserPrivateInfo.Username == Username && c.UserPrivateInfo.Password == Password)
+                .FirstOrDefault();
+
+            if (query != null)
+            {
+                query.UserPrivateInfo.Password = NewPassword;
+                return true;
+            } else { 
+                return false; 
+            }
+        }
+    }
+}
