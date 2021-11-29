46707c0c87924f68248dc7f80b2fed0617334361
linda <lindaserrano.r@gmail.com>
2021-11-29 13:57:21 +0100

    påbörjade Adminclient Issue#5

--- AdminClient/Program.cs
+++ AdminClient/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataLayer;
+
+Console.WriteLine("what would you like to do today?");
+Console.WriteLine("1. Reset database");
+Console.WriteLine("2. See user list");
+Console.WriteLine("3. See restaurant list");
+Console.WriteLine("4. Add new restaurant");
+
+var options = Console.ReadLine();
+
+if (options == "1")
+{
+    AdminBackend.PrepareDatabase();
+
+}
+
+
+
+
+if (options == "2")
+{
+
+
+}
+
+
+
+
+if (options == "3")
+{
+
+}
+
+
+
+
+if (options == "4")
+{
+
+}
