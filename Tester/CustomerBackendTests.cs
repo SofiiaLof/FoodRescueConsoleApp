b52aada8160ea426650c504277081630d46dfee8
hargaaya <hargaaya10@gmail.com>
2021-12-01 11:20:29 +0100

    Created a test and passed test for login method and purchasehistory.

--- Tester/CustomerBackendTests.cs
+++ Tester/CustomerBackendTests.cs
@@ -7,6 +7,7 @@ using DataLayer;
 using DataLayer.Data;
 using NuGet.Frameworks;
 using Xunit;
+using DataLayer.Backend;
 
 namespace Tester
 {
@@ -49,15 +50,43 @@ namespace Tester
             using var ctx = new FoodRescDbContext();
             AdminBackend.PrepareDatabase();
 
-            CustomerBackend customerBackend= new CustomerBackend();
+            CustomerBackend customerBackend = new CustomerBackend();
 
-            int  mealId = 9;
+            int mealId = 9;
             var user = customerBackend.TryLogin("anna23", "password1");
 
             var foodPackageToBuy = customerBackend.TryToBuyFoodPackage(mealId, user);
 
             Assert.NotNull(foodPackageToBuy);
             Assert.Equal(mealId, foodPackageToBuy.FoodPackageId);
+        }
+
+        [Fact]
+        public void PurchaseHistoryTest()
+        {
+            AdminBackend.PrepareDatabase();
+            using var ctx = new FoodRescDbContext();
+
+            CustomerBackend customerBackend = new CustomerBackend();
+            LoginManager login = new LoginManager();
+
+            var user = login.Login("bertil12", "password2");
+
+            // Kollar om användaren har köp från början, vilken den inte skall ha.
+
+            bool checkMeals = ctx.Sales
+                .Where(x => x.User.UserId == user.UserId).Any();
+
+            Assert.False(checkMeals);
+
+            customerBackend.TryToBuyFoodPackage(9, user);
+
+            // Kollar om användaren har nu köpt något målpaket.
+
+            bool check4Meals = ctx.Sales
+                .Where(x => x.User == user).Any();
+
+            Assert.True(check4Meals);
         }
 
     }
---
+++ Tester/LoginManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Data;
+using NuGet.Frameworks;
+using Xunit;
+using DataLayer.Backend;
+
+namespace Tester
+{
+    public class LoginManagerTests
+    {
+
+        [Fact]
+        public void LoginManagerTest()
+        {
+            AdminBackend.PrepareDatabase();
+            using var ctx = new FoodRescDbContext();
+
+            LoginManager loginManager = new LoginManager();
+
+            var getUser = ctx.UsersPrivateInfo
+                .Where(c => c.Username == "anna23").FirstOrDefault();
+
+            var loginUser = loginManager.Login("anna23", "password1").UserPrivateInfo;
+
+            Assert.True(getUser.EmailAddress == loginUser.EmailAddress);
+        }
+    }
+}
