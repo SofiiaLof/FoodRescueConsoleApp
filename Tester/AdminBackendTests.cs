f3ecfe4e5d5749be77474d5cb83eb5110ab2406e
linda <lindaserrano.r@gmail.com>
2021-12-02 17:25:03 +0100

    Fixade test till metoden CheckRegisteredRestaurant Issue#7

---
+++ Tester/AdminBackendTests.cs
@@ -0,0 +1,41 @@
+﻿using DataLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using Xunit;
+
+namespace Tester
+{
+    public class AdminBackendTests
+    {
+        [Fact]
+
+        public void CheckRegisteredRestaurantTests()
+        {
+
+            AdminBackend backend = new AdminBackend();
+            using var ctx = new FoodRescDbContext();
+            AdminBackend.PrepareDatabase();
+
+            var query = ctx.Restaurants;
+
+            var registeredRestaurants = query.ToList();
+
+            var registeredTest = backend.CheckRegisteredRestaurants();
+
+            Assert.NotNull(registeredTest);
+
+            Assert.Equal(registeredRestaurants.Count, registeredTest.Count);
+
+            Assert.Equal("Altans Pizzeria", registeredTest[0].RestaurantName);
+
+        }
+
+
+
+
+    }
+}
