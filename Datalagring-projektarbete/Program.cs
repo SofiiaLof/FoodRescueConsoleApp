0e036884aa58f98480c0a9774a63749c4ccc89e3
Sofiia Löf <sofiia.lof@learnet.se>
2021-12-03 09:38:34 +0100

    Städar lite koden tillsammans

--- Datalagring-projektarbete/Program.cs
+++ Datalagring-projektarbete/Program.cs
@@ -1,12 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using DataLayer;
-using DataLayer.Model;
-using DataLayer.Backend;
-using System.Diagnostics;
-using System.Threading;
-
-
-AdminBackend.PrepareDatabase();
-
-Console.WriteLine("Skapade databasen!");
+﻿
